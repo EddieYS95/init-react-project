deace7badcd795e211e17d070e0c22e9334608ca
Yunsang Jeong <kunka8271@gmail.com>
2021-09-21 01:26:18 +0900

    chore: add styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home: React.FC = () => {
-  return <>home</>;
+  return <Container>home</Container>;
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export default Home;
