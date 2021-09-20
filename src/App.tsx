cf568a961827112fbbd3eaad48b150a00347f002
Yunsang Jeong <kunka8271@gmail.com>
2021-09-21 01:14:32 +0900

    feat: complete init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  );
+};
+
+export default App;
